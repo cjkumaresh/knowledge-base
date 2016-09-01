c6efef542bd4472c79ac8c58d5066ae258568a66
kumaresh cj <cjkumaresh@gmail.com>
2016-09-01 23:46:46 +0530

    added arrow function in javascript concepts

---
+++ javascript/concepts/arrow-function.js
@@ -0,0 +1,19 @@
+// Reference https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+() => {}
+
+(() => {})() //IIFE
+
+let empty = () => {}
+
+let expression = () => 1 + 2;
+
+let return_something = () => { return 1+2 };
+
+let single_param_no_paranthesis = b => b;
+
+let return_object_literal_using_paranthesizing_body = () => ({ foo: 1 });
+
+
+
+ 
