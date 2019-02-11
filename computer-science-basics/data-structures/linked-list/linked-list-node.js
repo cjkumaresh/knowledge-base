059f25fc53e9803ec3e9c33b70a4bcb41532545d
apple <apple@apples-MacBook-Pro.local>
2019-02-11 07:31:26 +0530

    linked list find append prepend changes

--- computer-science-basics/data-structures/linked-list/linked-list-node.js
+++ computer-science-basics/data-structures/linked-list/linked-list-node.js
@@ -3,4 +3,8 @@ export default class LinkedListNode {
         this.value = value;
         this.next = next;
     }
+
+    toString(callback) {
+        return callback ? callback(this.value) : `${this.value}`;
+    }
 }
--- computer-science-basics/data-structures/linked-list/linked-list.js
+++ computer-science-basics/data-structures/linked-list/linked-list.js
@@ -1,3 +1,86 @@
+import LinkedListNode from "./linked-list-node";
+import Comparator from "../../utils/comparator";
+
 export default class LinkedList {
-    
+    constructor() {
+        this.head = null;
+        this.tail = null;
+
+        this.compare = new Comparator();
+    }
+
+    append(value) {
+        const newNode = new LinkedListNode(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+
+            return this;
+        }
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = new LinkedListNode(value, this.head);
+
+        this.head = newNode;
+
+        if (!this.tail) {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        return this;
+    }
+
+    toArray() {
+        const nodes = [];
+
+        let currentNode = this.head;
+
+        while (currentNode) {
+            nodes.push(currentNode);
+            currentNode = currentNode.next;
+        }
+
+        return nodes;
+    }
+
+    fromArray(values = []) {
+        values.map(value => this.append(value));
+    }
+
+    toString(callback) {
+        return this.toArray().map(node => node.toString(callback)).toString();
+    }
+
+    delete(value) {
+        return this;
+    }
+
+    findByValue(value) {
+        let currentNode = this.head;
+
+        while (currentNode) {
+            if (this.compare.equal(currentNode.value, value)) {
+                return currentNode;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        return null;
+    }
+
+    deleteHead() {
+        return this;
+    }
+
+    deleteTail() {
+        return this;
+    }
 }
