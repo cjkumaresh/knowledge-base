ec9fdde95134026e2ccf46acf8700e03cb61444f
cjkumaresh <4362517+cjkumaresh@users.noreply.github.com>
2019-02-25 21:36:42 +0530

    added babel script for debugging

--- computer-science-basics/algorithms/sorting/shell-sort/shell-sort.js
+++ computer-science-basics/algorithms/sorting/shell-sort/shell-sort.js
@@ -2,7 +2,7 @@ import Sort from '../sort';
 
 export default class ShellSort extends Sort {
     sort(originalArray) {
-        let array = [...originalArray]; //clone the array
+        let array = [...originalArray]; //1. clone the array
 
         let gap = Math.floor(array.length/2);
 
@@ -16,7 +16,7 @@ export default class ShellSort extends Sort {
                     if (this.comparator.lessThan(array[gapShiftedIndex], array[currentIndex])) {
                         [array[currentIndex], array[gapShiftedIndex]] = [array[gapShiftedIndex], array[currentIndex]];
                     }
-
+                    console.log("array in while loop", array);
                     gapShiftedIndex = currentIndex;
                     currentIndex -= gap; // recursively we have to compare with the already sorted elements in the same gap interval
                 }
@@ -25,7 +25,7 @@ export default class ShellSort extends Sort {
             }
             gap = Math.floor(gap/2);
         }
-
+        console.log("final array", array);
         return array;
     }
 
