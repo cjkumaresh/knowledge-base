e41b9f2b1711063a72674d0cf90365340cb53301
cjkumaresh <4362517+cjkumaresh@users.noreply.github.com>
2019-03-05 09:47:53 +0530

    added lint condif to repo

--- computer-science-basics/algorithms/sorting/selection-sort/selection-sort.js
+++ computer-science-basics/algorithms/sorting/selection-sort/selection-sort.js
@@ -1,30 +1,29 @@
-import Sort from '../sort'
+import Sort from '../sort';
 
 export default class SelectionSort extends Sort {
-    sort(originalArray) {
-        let array = [...originalArray];
-        const arrayLength = array.length;
-        let minIndex = 0;
+  sort(originalArray) {
+    const array = [...originalArray];
+    const arrayLength = array.length;
+    let minIndex = 0;
 
-        for (let i = 0; i < arrayLength - 1; i++) {
-            minIndex = i;
-            this.callbacks.visitingCallback(array[i]);
+    for (let i = 0; i < arrayLength - 1; i + 1) {
+      minIndex = i;
+      this.callbacks.visitingCallback(array[i]);
 
-            for (let j = i + 1; j < arrayLength; j++){
-                this.callbacks.visitingCallback(array[j]);
+      for (let j = i + 1; j < arrayLength; j + 1) {
+        this.callbacks.visitingCallback(array[j]);
 
-                if (this.comparator.lessThan(array[j], array[minIndex])) {
-                    minIndex = j;
-                }
-               
-            }
-
-            // we have found a new smallest element in the array
-            // and swap the element
-            if (minIndex !== i) {
-                [array[minIndex], array[i]] = [array[i], array[minIndex]];
-            }
+        if (this.comparator.lessThan(array[j], array[minIndex])) {
+          minIndex = j;
         }
-        return array;
+      }
+
+      // we have found a new smallest element in the array
+      // and swap the element
+      if (minIndex !== i) {
+        [array[minIndex], array[i]] = [array[i], array[minIndex]];
+      }
     }
+    return array;
+  }
 }
