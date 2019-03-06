181296cb6e68d954db1b447ba45ede029ed73211
cjkumaresh <4362517+cjkumaresh@users.noreply.github.com>
2019-03-06 09:59:35 +0530

    changed maxlength for lint

--- computer-science-basics/algorithms/sorting/shell-sort/shell-sort.js
+++ computer-science-basics/algorithms/sorting/shell-sort/shell-sort.js
@@ -1,32 +1,30 @@
 import Sort from '../sort';
 
 export default class ShellSort extends Sort {
-    sort(originalArray) {
-        let array = [...originalArray]; //1. clone the array
+  sort(originalArray) {
+    const array = [...originalArray]; // 1. clone the array
 
-        let gap = Math.floor(array.length/2);
+    let gap = Math.floor(array.length / 2);
 
-        while(gap > 0) {
-            for (let i = 0; i < (array.length - gap); i++) {
-                let currentIndex = i;
-                let gapShiftedIndex = i + gap;
+    while (gap > 0) {
+      for (let i = 0; i < (array.length - gap); i + 1) {
+        let currentIndex = i;
+        let gapShiftedIndex = i + gap;
 
-                while(currentIndex >=0) {
-                    this.callbacks.visitingCallback(array[currentIndex]);
-                    if (this.comparator.lessThan(array[gapShiftedIndex], array[currentIndex])) {
-                        [array[currentIndex], array[gapShiftedIndex]] = [array[gapShiftedIndex], array[currentIndex]];
-                    }
-                    // console.log("array in while loop", array);
-                    gapShiftedIndex = currentIndex;
-                    currentIndex -= gap; // recursively we have to compare with the already sorted elements in the same gap interval
-                }
-
-                
-            }
-            gap = Math.floor(gap/2);
+        while (currentIndex >= 0) {
+          this.callbacks.visitingCallback(array[currentIndex]);
+          if (this.comparator.lessThan(array[gapShiftedIndex], array[currentIndex])) {
+            [array[currentIndex], array[gapShiftedIndex]] = [array[gapShiftedIndex], array[currentIndex]];
+          }
+          // console.log("array in while loop", array);
+          gapShiftedIndex = currentIndex;
+          // recursively we have to compare with already sorted elements in the same gap interval
+          currentIndex -= gap;
         }
-        // console.log("final array", array);
-        return array;
+      }
+      gap = Math.floor(gap / 2);
     }
-
+    // console.log("final array", array);
+    return array;
+  }
 }
