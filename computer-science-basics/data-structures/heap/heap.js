5d942680a5db24287eb48f0856f63adf26949008
cjkumaresh <4362517+cjkumaresh@users.noreply.github.com>
2019-03-11 09:37:29 +0530

    heapify down changes

--- computer-science-basics/data-structures/heap/heap.js
+++ computer-science-basics/data-structures/heap/heap.js
@@ -9,6 +9,34 @@ export default class Heap {
     this.heapContainer = [];
   }
 
+  isEmpty() {
+    return !this.heapContainer.length;
+  }
+
+  hasLeftChild(index) {
+    return this.getLeftChildIndex(index) < this.heapContainer.length;
+  }
+
+  hasRightChild(index) {
+    return this.getRightChildIndex(index) < this.heapContainer.length;
+  }
+
+  static getLeftChildIndex(index) {
+    return (index * 2) + 1;
+  }
+
+  static getRightChildIndex(index) {
+    return (index * 2) + 2;
+  }
+
+  leftChild(index) {
+    return this.heapContainer[this.getLeftChildIndex(index)];
+  }
+
+  rightChild(index) {
+    return this.heapContainer[this.getRightChildIndex(index)];
+  }
+
   hasParent(currentIndex) {
     return this.getParentIndex(currentIndex) >= 0;
   }
@@ -22,6 +50,17 @@ export default class Heap {
     this.heapifyUp();
   }
 
+  poll() {
+    if (this.isEmpty) {
+      return null;
+    }
+    const item = this.heapContainer[0];
+
+    this.heapContainer[0] = this.heapContainer.pop();
+    this.heapifyDown();
+    return item;
+  }
+
   heapifyUp(index) {
     let currentIndex = index || this.heapContainer.length - 1;
 
@@ -30,6 +69,27 @@ export default class Heap {
       const parentIndex = this.getParentIndex(currentIndex);
       this.swap(currentIndex, parentIndex);
       currentIndex = parentIndex;
+    }
+  }
+
+  heapifyDown(customStartIndex = 0) {
+    let currentIndex = customStartIndex;
+
+    let nextIndex;
+    while (this.hasLeftChild(currentIndex)) {
+      if (this.hasRightChild(currentIndex)
+        && this.pairIsInCorrectOrder(this.rightChild(currentIndex), this.leftChild(currentIndex))) {
+        nextIndex = this.getRightChildIndex(currentIndex);
+      } else {
+        nextIndex = this.getLeftChildIndex(currentIndex);
+      }
+
+      if (this.pairIsInCorrectOrder(this.heapContainer[currentIndex], this.heapContainer[nextIndex])) {
+        break;
+      }
+
+      this.swap(currentIndex, nextIndex);
+      currentIndex = nextIndex;
     }
   }
 
